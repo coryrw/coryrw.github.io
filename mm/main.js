b4d1caa6820a2b50a657b064609303a3f57ff51f
Cory Ward <coryrw@gmail.com>
2023-06-21 17:01:43 +1200

    fixed csv-parse

--- mm/main.js
+++ mm/main.js
@@ -1,2 +1,5 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 console.log('boop');
+//const data = parseCsv(records, options);
+addEventListener("DOMContentLoaded", (event) => { alert('yeeeee!'); });
--- mm/main.ts
+++ mm/main.ts
@@ -1,1 +1,5 @@
 console.log('boop');
+
+import { parse as parseCsv} from './csv-parse/sync.js';
+//const data = parseCsv(records, options);
+addEventListener("DOMContentLoaded", (event) => { alert('yeeeee!')});
